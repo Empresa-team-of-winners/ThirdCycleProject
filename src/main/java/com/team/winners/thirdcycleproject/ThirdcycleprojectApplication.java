7ac09c0d7ed0ec694311595972938c7b35955e70
Macxxon <maicolalarcon93@gmail.com>
2022-08-23 20:49:01 -0500

    instalacion springboot en proyecto

---
+++ src/main/java/com/team/winners/thirdcycleproject/ThirdcycleprojectApplication.java
@@ -0,0 +1,13 @@
+package com.team.winners.thirdcycleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdcycleprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdcycleprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team/winners/thirdcycleproject/ThirdcycleprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.winners.thirdcycleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdcycleprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
